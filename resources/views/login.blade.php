ddb5f321adaeefa3f681ae5e98ddba9f15ebef77
Irfan Hakim <hakimirfan90@gmail.com>
2022-10-15 20:09:07 +0700

    update css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     @if(env('APP_ENV') == 'production')
-        <link href="https://cdn.statically.io/gh/irfanhkm/tk-2-grades/ffafee97/public/css/app.css" rel="stylesheet">
+        <link href="https://cdn.statically.io/gh/irfanhkm/tp-2-login/8e1a93ac/public/css/app.css" rel="stylesheet">
     @else
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @endif
